d6f7ee1a632b76efa77a7ea872cd47f6e29d7857
kalingay <y.kalinga@directfn.com>
2018-06-11 20:53:52 +0530

    response body class

---
+++ frontend_1/src/app/services/response-body.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by KalingaY on 6/11/2018.
+ */
+export class ResponseBody {
+    status: number;
+    success: boolean;
+    data: any;
+
+    constructor(values: Object = {}) {
+        Object.assign(this, values);
+    }
+}
